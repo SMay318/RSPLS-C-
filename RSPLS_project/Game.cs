2cc1a4991cd7f6f830ffaaf7b96fba96d063ea16
Sean May <s_may318@hotmail.com>
2021-11-16 15:31:26 -0500

    added to the human class where it takes in the Choosegesture function from its parent class. added run game to the Program Class, in the game class added the list of gestures as well as values for  a player 1 and 2 as wells as the starting round.

--- RSPLS_project/Game.cs
+++ RSPLS_project/Game.cs
@@ -8,5 +8,15 @@ namespace RSPLS_project
 {
     class Game
     {
+        Player player1;
+        Player player2;
+        List<string> gestures;
+        public int round;
+
+
+        public Game()
+        {
+            gestures = new List<string>() { "rock", "paper", "scissors", "Lizard", "spock" };
+            round = 1;
     }
 }
--- RSPLS_project/Human.cs
+++ RSPLS_project/Human.cs
@@ -8,5 +8,18 @@ namespace RSPLS_project
 {
     public class Human : Player
     {
+        public string type;
+
+
+        public Human()
+        {
+            type = "human";
+        }
+
+        public override string ChooseGesture(List<string> gestures, Player player)
+        {
+            string userGesture;
+              
+        }
     }
 }
